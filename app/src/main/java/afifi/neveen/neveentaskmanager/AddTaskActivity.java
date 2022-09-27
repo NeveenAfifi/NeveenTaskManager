6ce763ee8bb2463a00619598fec6e0de4804dbf9
Admin <neveen299@gmail.com>
2022-09-27 08:42:45 +0300

    gghjg

--- app/src/main/java/afifi/neveen/neveentaskmanager/AddTaskActivity.java
+++ app/src/main/java/afifi/neveen/neveentaskmanager/AddTaskActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * koko
+ */
 public class AddTaskActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+    }
+
+    /**
+     * dsdgsdg
+     * @param a ggggg
+     * @param x hhhhh
+     * @return kkkkk
+     */
+    public int dodo(double a, char x)
+    {
+ return 0;
     }
 }
--- app/src/main/java/afifi/neveen/neveentaskmanager/MainActivity.java
+++ app/src/main/java/afifi/neveen/neveentaskmanager/MainActivity.java
@@ -11,10 +11,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //تقوم ببناء شاشة التنسيق وكل الكائنات التي تحويها
         setContentView(R.layout.activity_main);
+        //crwat kjsdnfjsjdfkjsdfnkj
+        AddTaskActivity a = new AddTaskActivity();
     }
 }
